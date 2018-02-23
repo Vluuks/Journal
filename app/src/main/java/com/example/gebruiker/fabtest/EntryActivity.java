d3e0f3bb59852872b09596c0d0e493713e415d4a
Renske <vluuks@gmail.com>
2018-02-23 22:12:25 +0100

    added layout for activities

--- app/src/main/java/com/example/gebruiker/fabtest/EntryActivity.java
+++ app/src/main/java/com/example/gebruiker/fabtest/EntryActivity.java
@@ -2,6 +2,9 @@ package com.example.gebruiker.fabtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class EntryActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class EntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
+
+    }
+
+    /* Sets the mood that will be entered into the database. */
+    public void setMood(View view) {
+
+        ImageView imageButton = (ImageView) view;
+        switch(imageButton.getId()) {
+
+            case R.id.mood1:
+
+                break;
+            case R.id.mood2:
+
+                break;
+            case R.id.mood3:
+
+                break;
+            case R.id.mood4:
+
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/gebruiker/fabtest/MainActivity.java
+++ app/src/main/java/com/example/gebruiker/fabtest/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.gebruiker.fabtest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
+/* Activity that contains a list of entries and allows users to create new
+   entries through
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lv = findViewById(R.id.listView);
+    }
+
+    public void addEntry(View view) {
+        Intent intent = new Intent(MainActivity.this, EntryActivity.class);
+        startActivity(intent);
     }
 }
